da68f87b7adabf53724406302b9c3fc50c2b733e
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-02 09:45:07 -0400

    try with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { MetamaskProvider } from '@rarimo/providers-evm'
 import { useProvider } from '@rarimo/react-provider'
+import { useEffect } from 'react'
 
 function App() {
   const { provider } = useProvider(MetamaskProvider)
+  console.log(provider)
 
-  provider?.connect()
+  useEffect(() => {
+    if (provider) provider.connect()
+  }, [provider])
   console.log(provider?.address)
 
   return <div className="App">Wallet address: {provider?.address}</div>
